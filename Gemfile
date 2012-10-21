8578b0294d90818373db189d00f969e1275ac5eb
Shannon Rush <shannonmrush@gmail.com>
2012-10-21 11:02:29 -0600

    skeleton rails app, first commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.2.3'
+gem 'mysql2'
+
+gem 'haml-rails'
+gem 'sass'
+
+gem 'capistrano'
+
+gem 'jquery-rails'
+gem 'uglifier'
+gem 'therubyracer'
+
+group :development do
+ gem 'thin'
+ gem 'quiet_assets'
+ gem 'debugger'
+ gem "rspec-rails"
+end
+
+group :test do
+  gem "rspec-rails"
+  gem "factory_girl"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Epiccamcam::Application.config.secret_token = '148b95d6b4d08dfc9b1b1d620404a2b3cb50915ae72b7e5fc585061d5eb6397a07598dc2204c19a9f7cef329689a7b961afcb2b763708e208f8f0d96775206da'
